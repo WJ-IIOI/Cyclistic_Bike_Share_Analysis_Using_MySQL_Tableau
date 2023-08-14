af2828e784405390e06283087ac406c223d38134
Jia Wang <115512296+WJ-IIOI@users.noreply.github.com>
2023-08-13 23:33:33 -0500

    Update 02_trip_2022_query.sql

--- 02_trip_2022_query.sql
+++ 02_trip_2022_query.sql
@@ -28,17 +28,17 @@ FROM `trip_2022`;
 SELECT 
 	sum(isnull(ride_id)) ride_id,
 	sum(isnull(rideable_type)) rideable_type,
-    sum(isnull(started_at)) started_at,
-    sum(isnull(ended_at)) ended_at,
-    sum(isnull(start_station_name)) start_station_name,
-    sum(isnull(start_station_id)) start_station_id,
-    sum(isnull(end_station_name)) end_station_name,
-    sum(isnull(end_station_id)) end_station_id,
-    sum(isnull(start_lat)) start_lat,
-    sum(isnull(start_lng)) start_lng,
-    sum(isnull(end_lat)) end_lat,
-   	sum(isnull(end_lng)) end_lng,
-    sum(isnull(member_casual)) member_casual
+	sum(isnull(started_at)) started_at,
+	sum(isnull(ended_at)) ended_at,
+	sum(isnull(start_station_name)) start_station_name,
+	sum(isnull(start_station_id)) start_station_id,
+	sum(isnull(end_station_name)) end_station_name,
+	sum(isnull(end_station_id)) end_station_id,
+	sum(isnull(start_lat)) start_lat,
+	sum(isnull(start_lng)) start_lng,
+	sum(isnull(end_lat)) end_lat,
+	sum(isnull(end_lng)) end_lng,
+	sum(isnull(member_casual)) member_casual
 FROM `trip_2022`;
 
 -- Checking for the null values of start_station name and id whether same
@@ -47,19 +47,19 @@ FROM `trip_2022`;
 -- end_lat, end_lng total 5858
 SELECT 
 	start_station_name,
-    start_station_id,
+	start_station_id,
 	end_station_name, 
-    end_station_id,
-    end_lat,
+	end_station_id,
+	end_lat,
 	end_lng
 FROM trip_2022
 WHERE 
-	start_station_name is NULL AND
-    start_station_id is NULL AND
-	end_station_name is NULL AND
-    end_station_id is NULL AND
-	end_lat is NULL AND
-    end_lng is NULL;
+	start_station_name is NULL 
+	AND start_station_id is NULL 
+	AND end_station_name is NULL 
+	AND end_station_id is NULL 
+	AND end_lat is NULL 
+	AND end_lng is NULL;
     
 
 -- Checking for the null values of start_station name and id whether same
@@ -70,30 +70,28 @@ WHERE
 -- Most NULL station name with same lat ane lng, which means no values, need to delete
 SELECT 
 	start_station_name,
-    -- start_station_id
 	end_station_name, 
-    -- end_station_id
-    start_lat,
-    start_lng,
-    end_lat,
+	start_lat,
+	start_lng,
+	end_lat,
 	end_lng
 FROM trip_2022
 WHERE 
 	start_station_name is NULL
 	AND start_lat = 41.7900
-    AND start_lng = -87.6;
-    -- start_station_id is NULL
-	-- end_station_name is NULL AND
-    -- end_station_id is NULL AND
-    -- end_lat is NULL AND
-    -- end_lng is NULL;
+	AND start_lng = -87.6;
+-- start_station_id is NULL
+-- end_station_name is NULL AND
+-- end_station_id is NULL AND
+-- end_lat is NULL AND
+-- end_lng is NULL;
     
 SELECT 
 	timediff(ended_at, started_at) as ride_sec,
-    start_station_name,
-    end_station_name,
-    end_lat,
-    end_lng
+	start_station_name,
+	end_station_name,
+	end_lat,
+	end_lng
 FROM trip_2022
 WHERE 
 	timediff(ended_at, started_at) > TIME('24:00:00')
@@ -120,21 +118,24 @@ GROUP BY length_ride_id;
 SELECT started_at, ended_at, timediff(ended_at, started_at) 
 FROM trip_2022
 -- filter ride_length more than 1 day = 5360 rows
-WHERE timediff(ended_at, started_at) > TIME('24:00:00')
+WHERE 
+	timediff(ended_at, started_at) > TIME('24:00:00')
 ORDER BY 3 DESC;
 
 SELECT started_at, ended_at, timediff(ended_at, started_at) 
 FROM trip_2022
 -- filter ride_length more than 1 min = 121089 rows
-WHERE timediff(ended_at, started_at) < TIME('00:01:00')
+WHERE 
+	timediff(ended_at, started_at) < TIME('00:01:00')
 ORDER BY 3 DESC;
 
 -- 5156 rows 
 SELECT timediff(ended_at, started_at), end_lat, end_lng
 FROM trip_2022
-WHERE (timediff(ended_at, started_at) > TIME('24:00:00') 
+WHERE 
+	(timediff(ended_at, started_at) > TIME('24:00:00') 
 	OR timediff(ended_at, started_at) < TIME('00:01:00'))
-    AND (end_lat IS NULL AND end_lng IS NULL)
+	AND (end_lat IS NULL AND end_lng IS NULL)
 ORDER BY 1;
 
 
@@ -142,7 +143,7 @@ ORDER BY 1;
 SELECT 
 	member_casual, 
 	count(*) AS user,
-    count(*) / (SELECT COUNT(*) FROM trip_2022) AS percentage
+	count(*) / (SELECT COUNT(*) FROM trip_2022) AS percentage
 FROM trip_2022
 GROUP BY member_casual;
 
